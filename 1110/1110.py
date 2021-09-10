2293c43082ec4c2fb60942ac5140a4a997d9fa45
Zubayer Rahman <dev.zubayer@gmail.com>
2021-09-10 20:05:46 +0600

    space optimized solution added

--- 1110/1110.py
+++ 1110/1110.py
@@ -4,8 +4,8 @@
 # Author       : Zubayer Rahman
 # Email        : zubayer.csesust@gmail.com
 # Time Limit   : 5.000s
-# CPU          : 0.978s
-# Memory       : 3588KB
+# CPU          : 0.915s
+# Memory       : 3488KB
 # Algorithm    : Dynamic Programming (LCS)
 
 import sys
@@ -26,25 +26,50 @@ def minim(s1, s2) -> str:
     return s1
 
 
+# def LCS(s1, s2):
+#     n = len(s1)
+#     m = len(s2)
+#
+#     lcs = [["" for _ in range(m + 1)] for _ in range(n + 1)]
+#
+#     for i in reversed(range(n)):
+#         for j in reversed(range(m)):
+#             if i == n or j == m:
+#                 lcs[i][j] = ""
+#                 continue
+#
+#             if s1[i] == s2[j]:
+#                 lcs[i][j] = s1[i] + lcs[i + 1][j + 1]
+#                 continue
+#
+#             lcs[i][j] = minim(lcs[i][j + 1], lcs[i + 1][j])
+#
+#     return lcs[0][0]
+
+
+# space optimized version
 def LCS(s1, s2):
     n = len(s1)
     m = len(s2)
 
-    lcs = [["" for _ in range(m + 1)] for _ in range(n + 1)]
+    binary_index = bool
 
+    lcs = [["" for _ in range(m + 1)] for _ in range(2)]
     for i in reversed(range(n)):
+        binary_index = i & 1
+
         for j in reversed(range(m)):
             if i == n or j == m:
-                lcs[i][j] = ""
+                lcs[binary_index][j] = ""
                 continue
 
             if s1[i] == s2[j]:
-                lcs[i][j] = s1[i] + lcs[i + 1][j + 1]
+                lcs[binary_index][j] = s1[i] + lcs[1 - binary_index][j + 1]
                 continue
 
-            lcs[i][j] = minim(lcs[i][j + 1], lcs[i + 1][j])
+            lcs[binary_index][j] = minim(lcs[binary_index][j + 1], lcs[1 - binary_index][j])
 
-    return lcs[0][0]
+    return lcs[binary_index][0]
 
 
 def main():
